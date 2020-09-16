9a44df36bfa29b838b499a7612ec3f0c3a1dd80c
baba_s <shotababa1206@gmail.com>
2020-09-16 22:46:35 +0900

    first commit

---
+++ Scripts/StringReplaceRule.cs
@@ -0,0 +1,22 @@
+﻿namespace Kogane
+{
+	public readonly struct StringReplaceRule
+	{
+		public string OldValue { get; }
+		public string NewValue { get; }
+
+		public StringReplaceRule( string oldValue, string newValue )
+		{
+			OldValue = oldValue;
+			NewValue = newValue;
+		}
+	}
+
+	public static class StringExt
+	{
+		public static string Replace( this string self, StringReplaceRule rule )
+		{
+			return self.Replace( rule.OldValue, rule.NewValue );
+		}
+	}
+}
